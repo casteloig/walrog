d1a39486b9cbc18237409fa626b8b8199afc2a5a
Nacho Castelo <casteloig@outlook.es>
2024-07-03 23:53:20 +0200

    add more util functions

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -13,18 +13,23 @@ func CalculateCRC(data []byte) uint32 {
 	return crc32.ChecksumIEEE(data)
 }
 
-// Uint32ToBytes takes a uint32 and returns its conversion to []byte
-func Uint32ToBytes(number uint32) []byte {
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, number)
-	return buf
-}
-
 // IntToUint32 takes a integer and returns its conversion to uint32.
 // If negative returns an error. If overflow caused by int bigger than uint32 returns an error.
 func IntToUint32(i int) (uint32, error) {
 	if (i < 0) || (i > UINT32_MAX_NUMBER) {
-		return 0, errors.New("cannot convert negative int to uint32")
+		return 0, errors.New("overflow transforming int to uint32")
 	}
 	return uint32(i), nil
+}
+
+// numToBytes takes an integer and return its conversion to bytes.
+// The number has to be positive and within the range of uint32
+func Uint32ToBytes(i uint32) []byte {
+	buf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(buf, i)
+	return buf
+}
+
+func BytesToUint32(i []byte) uint32 {
+	return binary.LittleEndian.Uint32(i)
 }
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -31,15 +32,108 @@ func TestIntToUint32(t *testing.T) {
 	}
 }
 
-// Helper function to compare byte slices
-func equal(a, b []byte) bool {
-	if len(a) != len(b) {
-		return false
+func TestUint32ToBytes(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    uint32
+		expected []byte
+	}{
+		{
+			name:     "Zero",
+			input:    0,
+			expected: []byte{0, 0, 0, 0},
+		},
+		{
+			name:     "One",
+			input:    1,
+			expected: []byte{1, 0, 0, 0},
+		},
+		{
+			name:     "MaxUint32",
+			input:    4294967295,
+			expected: []byte{255, 255, 255, 255},
+		},
+		{
+			name:     "Arbitrary number",
+			input:    305419896, // 0x12345678
+			expected: []byte{120, 86, 52, 18},
+		},
 	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Uint32ToBytes(tc.input)
+			if !bytes.Equal(result, tc.expected) {
+				t.Errorf("Uint32ToBytes(%d) = %v; want %v", tc.input, result, tc.expected)
+			}
+		})
 	}
-	return true
+}
+
+func TestBytesToUint32(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    []byte
+		expected uint32
+	}{
+		{
+			name:     "Zero",
+			input:    []byte{0, 0, 0, 0},
+			expected: 0,
+		},
+		{
+			name:     "One",
+			input:    []byte{1, 0, 0, 0},
+			expected: 1,
+		},
+		{
+			name:     "MaxUint32",
+			input:    []byte{255, 255, 255, 255},
+			expected: 4294967295,
+		},
+		{
+			name:     "Arbitrary number",
+			input:    []byte{120, 86, 52, 18},
+			expected: 305419896, // 0x12345678
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := BytesToUint32(tc.input)
+			if result != tc.expected {
+				t.Errorf("Uint32ToBytes(%d) = %v; want %v", tc.input, result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestCalculateCRConBytes(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    []byte
+		expected []byte
+	}{
+		{
+			name:     "first example",
+			input:    []byte{1, 0, 0, 0, 3, 0, 0, 0, 1, 2, 3},
+			expected: []byte{190, 45, 28, 49},
+		},
+		{
+			name:     "second example",
+			input:    []byte{11, 0, 0, 0, 10, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []byte{181, 89, 81, 142},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			resultCRC := CalculateCRC(tc.input)
+			resultCRConBytes := Uint32ToBytes(resultCRC)
+			if !bytes.Equal(resultCRConBytes, tc.expected) {
+				t.Errorf("Uint32ToBytes(%d) = %v; want %v", tc.input, resultCRConBytes, tc.expected)
+			}
+		})
+	}
+
 }
